4bbe6fa3c130aae050655e71278f454f0e805e12
kalinessa <70555332+kalinessa@users.noreply.github.com>
2023-02-19 14:13:32 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+/*Formatting a collapsible section of text*/
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("activeCollapsable");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
